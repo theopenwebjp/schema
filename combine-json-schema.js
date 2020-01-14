3a98b6768abea3666375cbd4aee1dea131fc13d3
Damien Golding <noreply@damiengolding.com>
2020-01-14 15:20:26 +0900

    fixes

---
+++ combine-json-schema.js
@@ -0,0 +1,14 @@
+const $RefParser = require('json-schema-ref-parser')
+const schema = require('./schema/json-schema/index.json')
+const fs = require('fs-extra')
+const path = require('path')
+
+async function combine() {
+  const newSchema = await $RefParser.dereference(schema)
+  console.log('Combined schema:', newSchema)
+  return newSchema
+}
+
+combine().then(newSchema => {
+  return fs.writeJSON(path.join(__dirname, 'test', 'output', 'combined-schema.json'), newSchema)
+})
