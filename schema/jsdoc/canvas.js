935cbfabfefb2d38976277d4b6255b9e8b73b6a2
Damien Golding <noreply@damiengolding.com>
2020-10-16 13:01:37 +0900

    jsdoc common types

---
+++ schema/jsdoc/canvas.js
@@ -0,0 +1,13 @@
+/**
+* https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+* @typedef {Object} CircleRange
+* @property {number} startingAngle
+* @property {number} endingAngle
+*/
+
+/**
+ * ?: https://developer.mozilla.org/en-US/docs/Web/API/CSSImageValue
+ * @typedef {HTMLImageElement | SVGImageElement | HTMLVideoElement | HTMLCanvasElement | ImageBitmap | OffscreenCanvas} Drawable
+ */
+
+export const {}
---
+++ schema/jsdoc/index.js
@@ -0,0 +1,36 @@
+/**
+ * @typedef {Object} Size
+ * @property {number} width
+ * @property {number} height
+ */
+
+/**
+* @typedef {Object} Position
+* @property {number} x
+* @property {number} y
+*/
+
+/**
+ * @typedef {Position} Point ALIAS
+ */
+
+/**
+ * @typedef {Size & Position} Area
+ */
+
+ /**
+ * @typedef {Object} RGBASelection
+ * @property {boolean} r
+ * @property {boolean} g
+ * @property {boolean} b
+ * @property {boolean} a
+ */
+
+/**
+ * @typedef {object} SimpleDOMRect
+ * @property {number} top
+ * @property {number} bottom
+ * @property {number} left
+ * @property {number} right
+ */
+export const {}
